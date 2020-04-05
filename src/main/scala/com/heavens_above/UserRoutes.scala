ba83e8870a158781f4c27bff7413242fc8b7b976
jake ashdown <jake.ashdown@protonmail.com>
2020-04-05 13:49:30 +0200

    Add CORS handling to GraphQL endpoint
    This allows the frontend running at http://localhost:4200 to make
    requests to the endpoint.

--- src/main/scala/com/heavens_above/UserRoutes.scala
+++ src/main/scala/com/heavens_above/UserRoutes.scala
@@ -1,44 +1,36 @@
 package com.heavens_above
 
-import akka.http.scaladsl.server.Directives._
-import akka.http.scaladsl.model.StatusCodes
-import akka.http.scaladsl.server.Route
 import scala.concurrent.Future
 import scala.util.{Failure, Success}
 
-import com.heavens_above.UserRegistry._
-import akka.actor.typed.ActorRef
-import akka.actor.typed.ActorSystem
+import akka.actor.typed.{ActorRef, ActorSystem}
 import akka.actor.typed.scaladsl.AskPattern._
-import akka.util.Timeout
-import sangria.execution.{ErrorWithResolver, Executor, QueryAnalysisError}
-import sangria.parser.QueryParser
-import spray.json.{JsObject, JsString, JsValue}
-import sangria.ast.Document
-import sangria.execution.deferred.DeferredResolver
-import sangria.execution.{ErrorWithResolver, Executor, QueryAnalysisError}
-import sangria.parser.{QueryParser, SyntaxError}
-import sangria.parser.DeliveryScheme.Try
-import akka.http.scaladsl.Http
 import akka.http.scaladsl.model.StatusCodes._
 import akka.http.scaladsl.server.Directives._
-import akka.http.scaladsl.model.MediaTypes._
-import akka.http.scaladsl.server._
-import akka.stream.{ActorMaterializer, Materializer}
-import scala.util.control.NonFatal
-import scala.util.{Failure, Success}
+import akka.http.scaladsl.server.Route
+import akka.stream.Materializer
+import akka.util.Timeout
+import com.heavens_above.UserRegistry._
+import sangria.ast.Document
+import sangria.execution.{ErrorWithResolver, Executor, QueryAnalysisError}
+import sangria.parser.DeliveryScheme.Try
+import sangria.parser.QueryParser
+import spray.json.{JsObject, JsString, JsValue}
 
 //#import-json-formats
 //#user-routes-class
-class UserRoutes(userRegistry: ActorRef[UserRegistry.Command])(implicit val system: ActorSystem[_]) {
+class UserRoutes(userRegistry: ActorRef[UserRegistry.Command])(
+  implicit val system: ActorSystem[_]
+) extends CorsHandler {
 
   //#user-routes-class
   import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
-  import JsonFormats._
   //#import-json-formats
 
   // If ask takes more time than this to complete the request is failed
-  private implicit val timeout = Timeout.create(system.settings.config.getDuration("my-app.routes.ask-timeout"))
+  private implicit val timeout = Timeout.create(
+    system.settings.config.getDuration("my-app.routes.ask-timeout")
+  )
 
   def getUsers(): Future[Users] =
     userRegistry.ask(GetUsers)
@@ -56,13 +48,19 @@ class UserRoutes(userRegistry: ActorRef[UserRegistry.Command])(implicit val syst
   implicit val ec = system.executionContext
 
   def executeGraphQLQuery(query: Document, op: Option[String], vars: JsObject) =
-    Executor.execute(UserSchema.schema, query, (), variables = vars, operationName = op)
+    Executor
+      .execute(
+        UserSchema.schema,
+        query,
+        (),
+        variables = vars,
+        operationName = op
+      )
       .map(OK → _)
       .recover {
         case error: QueryAnalysisError ⇒ BadRequest → error.resolveError
         case error: ErrorWithResolver ⇒ InternalServerError → error.resolveError
       }
-
 
   def graphQLEndpoint(requestJson: JsValue) = {
     val JsObject(fields) = requestJson
@@ -89,17 +87,18 @@ class UserRoutes(userRegistry: ActorRef[UserRegistry.Command])(implicit val syst
     }
   }
 
-
   //#all-routes
   //#users-get-post
   //#users-get-delete
   val userRoutes: Route =
-  (post & path("graphql")) {
-    entity(as[JsValue]) { requestJson ⇒
-      graphQLEndpoint(requestJson)
+  corsHandler {
+    (post & path("graphql")) {
+      entity(as[JsValue]) { requestJson ⇒
+        graphQLEndpoint(requestJson)
+      } ~
+        get {
+          getFromResource("playground.html")
+        }
     }
-  } ~
-    get {
-      getFromResource("playground.html")
-    }
+  }
 }
