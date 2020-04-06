7861d5f06d7d4c371e12287659ac35f1cc288934
jake ashdown <jake.ashdown@protonmail.com>
2020-04-06 21:37:54 +0200

    Scalafmt project

--- src/main/scala/com/heavens_above/CorsHandler.scala
+++ src/main/scala/com/heavens_above/CorsHandler.scala
@@ -1,7 +1,7 @@
 package com.heavens_above
 
-import akka.http.scaladsl.model.{HttpResponse, StatusCodes}
-import akka.http.scaladsl.server.{Directive0, Route}
+import akka.http.scaladsl.model.{ HttpResponse, StatusCodes }
+import akka.http.scaladsl.server.{ Directive0, Route }
 
 trait CorsHandler {
 
@@ -12,12 +12,7 @@ trait CorsHandler {
   private val corsResponseHeaders = List(
     `Access-Control-Allow-Origin`.*,
     `Access-Control-Allow-Credentials`(true),
-    `Access-Control-Allow-Headers`(
-      "Authorization",
-      "Content-Type",
-      "X-Requested-With"
-    )
-  )
+    `Access-Control-Allow-Headers`("Authorization", "Content-Type", "X-Requested-With"))
 
   //this directive adds access control headers to normal responses
   private def addAccessControlHeaders: Directive0 = {
@@ -27,11 +22,7 @@ trait CorsHandler {
 
   //this handles preflight OPTIONS requests.
   private def preflightRequestHandler: Route = options {
-    complete(
-      HttpResponse(StatusCodes.OK).withHeaders(
-        `Access-Control-Allow-Methods`(OPTIONS, POST, PUT, GET, DELETE)
-      )
-    )
+    complete(HttpResponse(StatusCodes.OK).withHeaders(`Access-Control-Allow-Methods`(OPTIONS, POST, PUT, GET, DELETE)))
 
   }
 
--- src/main/scala/com/heavens_above/JsonFormats.scala
+++ src/main/scala/com/heavens_above/JsonFormats.scala
@@ -5,7 +5,7 @@ import com.heavens_above.UserRegistry.ActionPerformed
 //#json-formats
 import spray.json.DefaultJsonProtocol
 
-object JsonFormats  {
+object JsonFormats {
   // import the default encoders for primitive types (Int, String, Lists etc)
   import DefaultJsonProtocol._
 
